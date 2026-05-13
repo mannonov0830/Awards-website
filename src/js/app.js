b82ff194889f1dda60d70c86e11f62d72ff82d02
mannonov0830 <kamronbekmannonov0830@gmail.com>
2026-05-13 23:35:09 +0500

    Main

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,18 @@
-let logoReload = document.getElementById("reload")
+let logoReload = document.getElementById("reload");
+let exploreBtn = document.querySelector(".explore");
+let modal = document.getElementById("modal");
+let closeBtn=document.getElementById("closeBtn")
 
 logoReload.addEventListener("click", () => {
-    location.reload()
-})
+    location.reload();
+});
+
+exploreBtn.addEventListener("click", (e) => {
+    modal.classList.add("active");
+});
+
+window.addEventListener("click", (e) => {
+    if (e.target === modal) {
+        modal.classList.remove("active");
+    }
+});
