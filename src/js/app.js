a0a1905bd472c72755d72b4d5f0da183c72a939a
mannonov0830 <kamronbekmannonov0830@gmail.com>
2026-05-14 09:34:56 +0500

    main

--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,43 @@
 let logoReload = document.getElementById("reload");
 let exploreBtn = document.querySelector(".explore");
+let serchBtn = document.getElementById("serchBtn")
+let reloadBtn = document.getElementById("reloadBtn")
 let modal = document.getElementById("modal");
-let closeBtn=document.getElementById("closeBtn")
+let closeBtn = document.getElementById("closeBtn")
+let btns = document.querySelectorAll(".btnTop")
 
 logoReload.addEventListener("click", () => {
     location.reload();
 });
 
-exploreBtn.addEventListener("click", (e) => {
+reloadBtn.addEventListener("click", () => {
+    location.reload()
+})
+
+exploreBtn.addEventListener("click", () => {
     modal.classList.add("active");
 });
+
+serchBtn.addEventListener("click", () => {
+    modal.classList.add("active");
+});
+
+
 
 window.addEventListener("click", (e) => {
     if (e.target === modal) {
         modal.classList.remove("active");
     }
+});
+
+btns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+
+        btns.forEach((item) => {
+            item.classList.remove("active");
+        });
+
+        btn.classList.add("active");
+
+    });
 });
